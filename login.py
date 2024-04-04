54cf5ddf7f7ad10706d473a28e0a2ca6e4f42a8f
coolpancakes <73265375+coolpancakes@users.noreply.github.com>
2024-04-04 12:03:50 +1100

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,34 @@
 # Login function for pools password manager
 
-import cryptography
+from cryptography.fernet import Fernet 	 
 import hashlib
+import functions 
+import time
 
 def login():
-	pass
+	functions.clear_screen()
+	print('''    
+   	
+
+    8""""8    l o g i n      login      ## USE GENERATED KEY! ##  
+    8    8 eeeee eeeee e     eeeee L
+    8eeee8 8  88 8  88 8     8   " O 	 
+    88     8   8 8   8 8e    8eeee G		  
+    88     8   8 8   8 88       88 I	
+    88     8eee8 8eee8 88eee 8ee88 N
+
+
+
+		''')
+	with open(r'C:\Users\h0hr\Secrets\login_hash.txt', 'r') as secret:
+		hashed = secret.readline()
+
+	try:
+		key = input('    | Enter KEY: ')
+		key_ = Fernet(key)
+
+		data = key_.decrypt(bytes(hashed.encode()))
+		#print(help(Fernet.decode()))
+		print('\n     SUCCESS!')
+	except:
+		print('\n     Invalid KEY!')
